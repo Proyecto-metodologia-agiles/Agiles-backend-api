10866c34e3739f5eb07ffce3c7e9634783276d63
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-10-06 22:31:45 -0500

    proyecto base

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,31 @@
+﻿using Application;
+using Infrastructure;
+using Infrastructure.Base;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*var optionsSqlServer = new DbContextOptionsBuilder<BancoContext>()
+             .UseSqlServer("Server=.\\;Database=Banco;Trusted_Connection=True;MultipleActiveResultSets=true")
+             .Options;*/
+           /*
+            var optionsInMemory = new DbContextOptionsBuilder<BancoContext>()
+             .UseInMemoryDatabase("Banco")
+             .Options;
+
+            BancoContext context = new BancoContext(optionsInMemory);
+            
+            
+           */
+        }
+
+        
+
+       
+    }
+}
---
+++ Domain/Contracts/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Contracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IAsesoriaRepository AsesoriaRepository { get; }
+        IAsesorRepository AsesorRepository { get; }
+        IEstudianteRepository EstudianteRepository { get; }
+        IMiembroComiteRepository MiembroComiteRepository { get; }
+        IObservacionRepository ObservacionRepository { get; }
+        IProyectoRepository ProyectoRepository { get; }
+        IValoracionRepository ValoracionRepository { get; }
+        int Commit();
+    }
+}
