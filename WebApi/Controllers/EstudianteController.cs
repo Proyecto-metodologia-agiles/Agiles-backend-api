a5e667f6f6afe095d17319e82dd2e358474da723
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-10-10 12:22:48 -0500

    creacion de controlador estudiante en (webApi-Controller)

---
+++ WebApi/Controllers/EstudianteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application;
+using Domain.Contracts;
+using Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudianteController : ControllerBase
+    {
+        readonly ProyectoContext _context;
+        readonly IUnitOfWork _unitOfWork;
+
+        //Se Recomienda solo dejar la Unidad de Trabajo
+        public EstudianteController(ProyectoContext context, IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult<CrearEstudianteResponse> Post(CrearEstudianteRequest request)
+        {
+            CrearEstudianteServive _service = new CrearEstudianteServive(_unitOfWork);
+            CrearEstudianteResponse response = _service.GuardarEstudiante(request);
+            return Ok(response);
+        }
+
+    }
+}
