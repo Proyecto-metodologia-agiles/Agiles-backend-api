25a0f8c42187c6047be21fb0cbb08e2b0ee76729
kevinjal <kevin.20099@hotmail.com>
2020-10-11 11:31:39 -0500

    Se creo el repositorio de los asesores

---
+++ Infrastructure/Repositories/AsesorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.Base;
+using System.Collections.Generic;
+
+namespace Infrastructure.Repositories
+{
+     public class AsesorRepository: GenericRepository<Asesor>, IAsesorRepository
+    {
+        public AsesorRepository(IDbContext context)
+             : base(context)
+        {
+
+        }
+    }
+}
