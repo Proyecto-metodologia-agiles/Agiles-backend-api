a3a697b854d2af7bd904ffd61b5baae7ee717518
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-10-12 15:42:30 -0500

    correcion rquest memmercommite

--- Application/Handles/Commite/CreateCommitteeMemberRequest.cs
+++ Application/Handles/Commite/CreateCommitteeMemberRequest.cs
@@ -9,34 +9,21 @@ namespace Application.Handles.Commite
     public class CreateCommitteeMemberRequest
     {
       
-        [MaxLength(50)]
-        [MinLength(3)]
-        [Required]
+        
         public string FullName { set; get; } = string.Empty;
 
-        
-        [MaxLength(50)]
-        [MinLength(3)]
-        [Required]
-        [EmailAddress]
+      
         public string Email { set; get; } = string.Empty;
 
         
-        [MaxLength(10)]
-        [MinLength(5)]
-        [Required]
-        [Phone]
+        
         public string Phone { set; get; } = string.Empty;
 
         
-        [MaxLength(15)]
-        [MinLength(1)]
-        [Required]
+      
         public string Password { set; get; } = string.Empty;
 
-        [MaxLength(15)]
-        [MinLength(1)]
-        [Required]
+       
         public string Identification { set; get; } = string.Empty;
     }
 }
