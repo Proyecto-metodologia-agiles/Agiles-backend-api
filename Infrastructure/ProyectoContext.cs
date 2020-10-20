2dae0e5b7cd3a24a8b6c4800d548742c110a239f
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-10-19 23:38:15 -0500

    context configurations

--- Infrastructure/ProyectoContext.cs
+++ Infrastructure/ProyectoContext.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace Infrastructure
 {
-    public class ProyectoContext : DbContext
+    public class ProyectoContext : DbContextBase
     {
 
         
@@ -26,21 +26,21 @@ namespace Infrastructure
         {
         }
 
-        /*
+        
         
         public ProyectoContext(DbContextOptions options) : base(options)
         {
 
         }
-        */
         
         
+        /*
          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-FDBCSN1\SQLEXPRESS;Database=ProyectoBD;Integrated Security=True;");
         } 
         
-        
+        */
         
 
     }
