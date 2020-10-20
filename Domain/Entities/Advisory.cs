4e1dc03e71f44047889764d7a68d6a903dab8486
fabiandres2000 <39952571+fabiandres2000@users.noreply.github.com>
2020-10-19 23:14:21 -0500

    create class advisory

---
+++ Domain/Entities/Advisory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Base;
+
+namespace Domain.Entities
+{
+    public class Advisory : Entity<int>
+    {
+        public Proyecto Proyect { get; set; }
+        public Asesor Thematic_Advisor { get; set; }
+        public Asesor Metodologic_Advisor { get; set; }
+        public int AssignedHours { get; set; }
+        public String semester { get; set; }
+        public String Year { get; set; }
+
+        public Advisory()
+        {
+            this.AssignedHours = 0;
+        }
+
+        public int Build_advisory(Advisory advisory)
+        {
+            if (advisory.Proyect==null || advisory.semester==null || advisory.Thematic_Advisor==null || advisory.Year==null || advisory.Metodologic_Advisor==null || advisory.AssignedHours==0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
