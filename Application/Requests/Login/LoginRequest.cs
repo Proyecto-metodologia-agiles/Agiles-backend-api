67aed78828359ba09384d86ddcbb35861058fd10
kevinjal <kevin.20099@hotmail.com>
2020-10-26 20:57:02 -0500

    Se crearon las clases de response y request en el login

---
+++ Application/Requests/Login/LoginRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Requests.Login
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public LoginRequest() 
+        {
+        }
+
+        public LoginRequest(string username, string password) 
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+
+
+
+    }
+}
---
+++ Application/Response/Login/LoginResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Response.Login
+{
+    public class LoginResponse
+    {
+
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Idetification { get; set; }
+        public string Telephone { get; set; }
+        public string Message { get; set; }
+        public LoginResponse() 
+        {
+        }
+
+        public LoginResponse(string id, string type, string name, string email, string identification, string telephone, string message) 
+        {
+            this.Id = id;
+            this.Type = type;
+            this.Name = name;
+            this.Email = email;
+            this.Idetification = identification;
+            this.Telephone = telephone;
+            this.Message = message;
+            
+        }
+    }
+}
