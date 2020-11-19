820f2f6315d56e8b0b58baf96cb7bbba6ad81114
kevinjal <kevin.20099@hotmail.com>
2020-11-18 21:27:26 -0500

    Se creo el servicio listar evaluaciones

---
+++ Application/Services/Evaluations/ConsultEvaluationService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Contracts;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http.Features;
+using System.Linq;
+
+namespace Application.Services.Evaluations
+{
+    public class ConsultEvaluationService
+    {
+        readonly IUnitOfWork _unitOfWork;
+
+        public ConsultEvaluationService (IUnitOfWork unitOfWork) 
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public List<Evaluacion> GetId(int id)
+        {
+            var val = _unitOfWork.EvaluationRepository.FindBy(t => t.Project.Id == id,includeProperties: "Project");
+            _unitOfWork.Dispose();
+            List<Evaluacion> evaluacions = new List<Evaluacion>();
+            foreach (var itemlist in val.ToList())
+            {
+                if (itemlist.ProjectId == id)
+                {
+                    evaluacions.Add(itemlist);
+                }
+            }
+            return evaluacions;
+        }
+    }
+}
